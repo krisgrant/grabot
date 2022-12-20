193cf20ae61a4e905d1057abe73e92058f768c44
KrisGra <39800337+krisgrant@users.noreply.github.com>
2022-12-20 17:59:46 +0200

    add github link command

---
+++ commands/github.js
@@ -0,0 +1,18 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('source')
+		.setDescription('Sends the bot\'s Github link!'),
+	async execute(interaction) {
+		const embed = new EmbedBuilder()
+			.setTitle('GitHub')
+			.setURL('https://github.com/krisgrant/grabot')
+			.setColor('#161B22');
+		// interaction.guild is the object representing the Guild in which the command was run
+		await interaction.reply({
+			content : 'This project is on Github! Go check it out!',
+			embeds: [embed],
+		});
+	},
+};
