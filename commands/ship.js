1a9ff98898754c262a25da5b1d455390a34a0451
KrisGra <39800337+krisgrant@users.noreply.github.com>
2022-12-20 17:27:58 +0200

    Create ship command and fix ping command

---
+++ commands/ship.js
@@ -0,0 +1,22 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('ship')
+		.setDescription('Input 2 people and it will give you a (totally not random) percentage of how well they fit together')
+		.addStringOption(option =>
+			option.setName('1st_person')
+				.setDescription('Person to be shipped with someone')
+				.setRequired(true))
+		.addStringOption(option =>
+			option.setName('2nd_person')
+				.setDescription('Person who you want to ship them with')
+				.setRequired(true)),
+	async execute(interaction) {
+		const firstPerson = interaction.options.getString('1st_person');
+		const secondPerson = interaction.options.getString('2nd_person');
+		const randomNum = Math.floor(Math.random() * 100);
+
+		await interaction.reply(`**${firstPerson}** and **${secondPerson}** are **${randomNum}%** Compatible!`);
+	},
+};
